05bc5056a29b11b94a85f2fa7bc2757d6bd7f27b
Peter van den Dungen <vddungenpeter@gmail.com>
2020-07-25 13:50:54 +0200

    init
    Init (experimental) project

---
+++ ConsoleToTortoiseTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using Mercurial;
+using Mercurial.Gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleToTortoiseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercurial.Repository repo = new Repository(@"C:\Users\vddungenpeter\source\repos\Test");
+
+            RevSpec working = repo.Identify();
+
+            //foreach (var changeSet in repo.Log())
+            //{
+            //    Console.WriteLine(changeSet);
+            //    if (changeSet.Revision == working)
+            //    {
+            //        Console.WriteLine("*** Working changeset!");
+            //    }
+            //}
+
+
+
+            //Mercurial.Gui.GuiClient.ClientType = GuiClientType.PyQT;
+            var cmd = new WriteGuiConsole();
+            Mercurial.Client.SetClientPath(@"C:\Program Files\TortoiseHG");
+            if (Client.CouldLocateClient)
+            {
+                repo.Execute(cmd);
+                //Client.Execute(new RevertCommand());
+            }
+
+            //Mercurial.Gui.GuiClient.InitGui(repo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleToTortoiseTest/WriteGuiConsole.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mercurial;
+using Mercurial.Gui;
+
+namespace ConsoleToTortoiseTest
+{
+
+    class WriteGuiConsole : IMercurialCommand
+    {
+        public string Command => "WriteToTortoise";
+
+        public IEnumerable<string> Arguments => new string[] { "Can+you+receive+me?" };
+
+        public Collection<string> AdditionalArguments => new Collection<string>();
+
+        public IMercurialCommandObserver Observer => new DebugObserver();
+
+        public int Timeout => 60;
+
+        public void After(int exitCode, string standardOutput, string standardErrorOutput)
+        {
+            Console.Out.WriteLine($"exitCode: {exitCode}");
+            Console.Out.WriteLine($"standardOutput: {standardOutput}");
+            Console.Out.WriteLine($"{standardErrorOutput}");
+        }
+
+        public void Before()
+        {
+            //Console.Out.WriteLine($"Before..");
+        }
+
+        public void Validate()
+        {
+            //Console.Out.WriteLine($"Validate..");
+        }
+    }
+
+    public class CustomDebugObserver : IMercurialCommandObserver
+    {
+        public void ErrorOutput(string line)
+        {
+            Console.WriteLine($"ErrorOutput: {line}");
+        }
+
+        public void Executed(string command, string arguments, int exitCode, string output, string errorOutput)
+        {
+            Console.WriteLine($"command: {command}");
+            Console.WriteLine($"arguments: {arguments}");
+            Console.WriteLine($"exitCode: {exitCode}");
+            Console.WriteLine($"output: {output}");
+            Console.WriteLine($"errorOutput: {errorOutput}");
+        }
+
+        public void Executing(string command, string arguments)
+        {
+            Console.WriteLine($"command: {command}");
+            Console.WriteLine($"arguments: {arguments}");
+        }
+
+        public void Output(string line)
+        {
+            Console.WriteLine($"ErrorOutput: {line}");
+        }
+    }
+}
